92950b287fea1c16b494fda4c32a6df394a9e5f3
Alan Charles <alanjcharles14@gmail.com>
2018-04-11 18:44:35 -0600

    add ten day forecast and minor styling

--- src/HourCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '.WeatherCard.css'
-
-const Card = () => {
-  return(
-    <div className="weather-card"></div>
-  )
-}
---
+++ src/TenDay.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react'
+import './TenDay.css'
+
+
+class TenDay extends Component {
+  constructor(props) {
+    super()
+
+  }
+
+  render() {
+    return(
+      <div className="day-container">
+      <section className="day-card">
+        <h4 className="day-date">April 15</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 16</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 17</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 18</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 19</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 20</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 21</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 22</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 23</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      <section className="day-card">
+        <h4 className="day-date">April 24</h4>
+        <h5 className="image-desc">I'm an image</h5>
+        <h4 className="day-projected">60°</h4>
+      </section>
+      </div>
+    )
+  }
+}
+
+export default TenDay
