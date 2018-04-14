a7a125eaea2158516a6e02b6e2419f119bc5bb99
Alan Charles <alanjcharles14@gmail.com>
2018-04-14 16:53:00 -0600

    full render of current weather and 7 hour from API

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,29 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './CurrentWeather.css'
 
-class CurrentWeather extends Component {
-  constructor(props) {
-    super()
-
-  }
-  render() {
+export default function CurrentWeather(props){
   return (
-    <section className="curr-container">
-      <h2 className="curr-location">{this.props.currLocation}</h2>
-      <div className="curr-data-container">
-        <div className="curr-temp">
-          <h3 className ="curr-date">{this.props.currDate}</h3>
-          <h3>{this.props.currCondition}</h3>
-        </div>
-        <div className="curr-condition">
-          <h3>{this.props.currTemp}</h3>
-          <p>{this.props.currHigh}</p>
-          <p>{this.props.currLow}</p>
-        </div>
-      </div>
-    </section>
+    <div>
+          <section className="curr-container">
+          <h2 className="curr-location">{props.cleanData.currentWeather.location}</h2>
+          <div className="curr-data-container">
+            <div className="curr-temp">
+              <h3 className ="curr-date">{props.cleanData.currentWeather.month}, {props.cleanData.currentWeather.day}</h3>
+              <img alt="current weather condition"className="image-desc" src= {props.cleanData.currentWeather.currentImage}/>
+              <h3>{props.cleanData.currentWeather.condition}</h3>
+            </div>
+            <div className="curr-condition">
+              <h3>50</h3>
+              <p>{props.cleanData.currentWeather.currentHigh}</p>
+              <p>{props.cleanData.currentWeather.currentLow}</p>
+            </div>
+          </div>
+        </section>
+    </div>
   )
 }
-}
 
-export default CurrentWeather
+
--- src/cleanData.js
+++ src/cleanData.js
@@ -1,46 +1,71 @@
-const getCurrLocation = (data) => {
-  return data.current_observation.display_location.full
+
+export default function cleanData(data) {
+  const cleanData = {
+    currentWeather: {
+        location: data.current_observation.display_location.full,
+        month: data.forecast.simpleforecast.forecastday[0].date.monthname,
+        date: data.forecast.txt_forecast.forecastday[0].title,
+        day: data.forecast.simpleforecast.forecastday[0].date.day,
+        currentHigh: data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+        currentLow: data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+        condition: data.current_observation.weather,
+        summary: data.forecast.txt_forecast.forecastday[0].fcttext,
+        currentImage: data.current_observation.icon_url
+      },
+    sevenHour: data.hourly_forecast.slice(0,7).map((hour) => {
+      return {
+        hours: hour.FCTTIME.civil,
+        temps: hour.temp.english,
+        images: hour.icon_url
+      }
+    })
+  }
+  return cleanData
 }
 
-const getCurrDate = (data) => {
-  const {pretty} = data.forecast.simpleforecast.forecastday[0].date
-  return pretty.split(' ').slice(4).join(' ')
-}
-
-const getCurrCondition = (data) => {
-  return data.forecast.simpleforecast.forecastday[0].conditions
-}
-
-const getCurrHigh = (data) => {
-  const {fcttext} = data.forecast.txt_forecast.forecastday[0]
-  return fcttext.split(' ').slice(0, 6).join(' ')
-}
-
-const getCurrLow = (data) => {
-  const {fcttext} = data.forecast.txt_forecast.forecastday[1]
-  return fcttext.split(' ').slice(0, 4).join(' ')
-}
-
-const getCurrTemp = (data) => {
-  return data.current_observation.temperature_string
-}
-
-const getHourData = (data) => {
-  const getHours = data.hourly_forecast.map(obj => 
-    obj.FCTTIME.hour).slice(0,7)
-  const getHourTemps = data.hourly_forecast.map(obj => 
-    obj.temp.english).slice(0,7)
-  const getHourImg = data.hourly_forecast.map(obj =>
-    obj.icon_url).slice(0,7)
-    const hourData = {hours:getHours, temp:getHourTemps, image:getHourImg}
-   return hourData
-}
-
-
-export {getCurrLocation,
-        getCurrDate,
-        getCurrCondition,
-        getCurrHigh,
-        getCurrLow,
-        getCurrTemp,
-       getHourData}
+// data.forecast.simpleforecast.forecastday.map
+// 
+// month: day.date.monthname,
+// day: day.date.day,
+// currentHigh: day.high.fahrenheit,
+// currentLow: day.low.fahrenheit,
+// condition: day.conditions,
+// currentImage: day.icon_url
+// 
+// const getCurrLocation = (data) => {
+  //   return data.current_observation.display_location.full
+  // }
+  
+  // const getCurrDate = (data) => {
+    //   const {pretty} = data.forecast.simpleforecast.forecastday[0].date
+    //   return pretty.split(' ').slice(4).join(' ')
+    // }
+    
+    // const getCurrCondition = (data) => {
+      //   return data.forecast.simpleforecast.forecastday[0].conditions
+      // }
+      
+      // const getCurrHigh = (data) => {
+        //   const {fcttext} = data.forecast.txt_forecast.forecastday[0]
+        //   return fcttext.split(' ').slice(0, 6).join(' ')
+        // }
+        
+        // const getCurrLow = (data) => {
+          //   const {fcttext} = data.forecast.txt_forecast.forecastday[1]
+          //   return fcttext.split(' ').slice(0, 4).join(' ')
+          // }
+          
+          // const getCurrTemp = (data) => {
+            //   return data.current_observation.temperature_string
+            // }
+            
+            // CurrentWeather: {
+              // date: data.forecast.simpleforecast.forecastday[0].date
+              //   return pretty.split(' ').slice(4).join(' ');
+              // condition: data.forecast.simpleforecast.forecastday[0].conditions;
+              // currentHigh: data.forecast.txt_forecast.forecastday[0].fcttext
+              //   return fcttext.split(' ').slice(0, 6).join(' ');
+              // currentLow:data.forecast.txt_forecast.forecastday[1].fcttext
+              //   return fcttext.split(' ').slice(0, 4).join(' ');
+              // currentTemp: data.current_observation.temperature_string;
+              // }
