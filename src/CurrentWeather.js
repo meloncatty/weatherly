6295932bcf8e8724f69aeb55577961508461301c
Alan Charles <alanjcharles14@gmail.com>
2018-04-15 19:43:35 -0600

    Add styling overhaul with new color scheme

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -7,14 +7,14 @@ export default function CurrentWeather(props){
     <h2 className="curr-location">{props.cleanData.currentWeather.location}</h2>
     <div className="curr-data">
       <div className="curr-left">
-        <h3 className ="curr-date">{props.cleanData.currentWeather.date} <br/> {props.cleanData.currentWeather.month}, {props.cleanData.currentWeather.day}</h3>
+        <h3 className ="curr-date">{props.cleanData.currentWeather.date} {props.cleanData.currentWeather.month}, {props.cleanData.currentWeather.day}</h3>
         <img alt="current weather condition"className="day-img" src={props.cleanData.currentWeather.currentImage}/>
         <h3>{props.cleanData.currentWeather.condition}</h3>
       </div>
       <div className="curr-right">
-        <p>{props.cleanData.currentWeather.summary}</p>
-        <p>High of {props.cleanData.currentWeather.currentHigh}°</p>
-        <p>Low of {props.cleanData.currentWeather.currentLow}°</p>
+        <p className="day-summary">{props.cleanData.currentWeather.summary}</p>
+        <h3 className="day-high-low">High {props.cleanData.currentWeather.currentHigh}° Low {props.cleanData.currentWeather.currentLow}°</h3>
+        {/* <p>Low of {props.cleanData.currentWeather.currentLow}°</p> */}
       </div>
     </div>
   </section>
